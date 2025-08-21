507e4a395b82622a49763bb844973ac0cbbbd392
David Ko <davidsj.ko@gmail.com>
2025-08-21 09:43:10 -0400

    Refactor

--- how-to-unit-test-a-function-that-uses-ticker/ticker.go
+++ how-to-unit-test-a-function-that-uses-ticker/ticker.go
@@ -9,15 +9,33 @@ import (
 type (
 	Printer struct {
 		Iteration int
+		Clock     Ticker
+	}
+
+	Ticker interface {
+		C() <-chan time.Time
+		Stop()
+	}
+
+	realClock struct {
+		t *time.Ticker
 	}
 )
+
+func (r *realClock) C() <-chan time.Time {
+	return r.t.C
+}
+
+func (r *realClock) Stop() {
+	r.t.Stop()
+}
 
 func (p *Printer) Start(ctx context.Context, done chan struct{}) {
 	defer p.Clock.Stop()
 
 	for {
 		select {
-		case <-t.C:
+		case <-p.Clock.C():
 			// Directly tied to wall clock, hard to control in tests
 			fmt.Println("tick at", time.Now())
 			p.Iteration++
